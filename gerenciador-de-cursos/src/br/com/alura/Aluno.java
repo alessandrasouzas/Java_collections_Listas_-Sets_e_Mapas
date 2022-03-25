111298b9bac1417d074224c102fd5169d325df53
Alessandra Souza <le16bits@gmail.com>
2022-03-25 13:28:59 -0300

    implementa add alunos e imprime conteudo da colecao utilizando foreach

--- gerenciador-de-cursos/src/br/com/alura/Aluno.java
+++ gerenciador-de-cursos/src/br/com/alura/Aluno.java
@@ -2,19 +2,24 @@ package br.com.alura;
 
 public class Aluno {
 
-    private String nome;
-    private int numeroMatricula;
+	private String nome;
+	private int numeroMatricula;
 
-    public Aluno(String nome, int numeroMatricula) {
-        this.nome = nome;
-        this.numeroMatricula = numeroMatricula;
-    }
+	public Aluno(String nome, int numeroMatricula) {
+		this.nome = nome;
+		this.numeroMatricula = numeroMatricula;
+	}
 
-    public String getNome() {
-        return nome;
-    }
+	public String getNome() {
+		return nome;
+	}
 
-    public int getNumeroMatricula() {
-        return numeroMatricula;
-    }
+	public int getNumeroMatricula() {
+		return numeroMatricula;
+	}
+
+	@Override
+	public String toString() {
+		return "[Aluno: " + this.nome + ", matricula: " + this.numeroMatricula + "]";
+	}
 }
