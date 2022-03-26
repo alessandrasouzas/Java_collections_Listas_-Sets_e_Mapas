bb09c369570921ebdea4d98111235399e47d0e15
Alessandra Souza <le16bits@gmail.com>
2022-03-25 21:56:20 -0300

    Iteração de uma coleção utilizando o Iterator

--- gerenciador-de-cursos/src/br/com/alura/TestaCursoComAluno.java
+++ gerenciador-de-cursos/src/br/com/alura/TestaCursoComAluno.java
@@ -1,5 +1,8 @@
 package br.com.alura;
 
+import java.util.Iterator;
+import java.util.Set;
+
 public class TestaCursoComAluno {
 
 	public static void main(String[] args) {
@@ -18,25 +21,36 @@ public class TestaCursoComAluno {
 		javaColecoes.matricula(a2);
 		javaColecoes.matricula(a3);
 
-		System.out.println("Todos os alunos matriculados: ");
+		System.out.println("\nTodos os alunos matriculados: ");
 		javaColecoes.getAlunos().forEach(aluno -> {
 			System.out.println(aluno);
 		});
 		
-		System.out.println("O aluno " + a1.getNome() + " está matriculado?");
+		System.out.println("\n--- TESTANDO CODIGO LEGADO: ITERATOR ---");
+		
+		Set<Aluno> alunos = javaColecoes.getAlunos();
+		Iterator<Aluno> iterador = alunos.iterator();
+		while(iterador.hasNext()) {
+			Aluno proximo = iterador.next();
+			System.out.println(proximo);
+		}
+		
+		System.out.println("\nO aluno " + a1.getNome() + " está matriculado?");
 		System.out.println(javaColecoes.estaMatriculado(a1));
 				
 		Aluno turini = new Aluno("Rodrigo Turini", 34672);
-		System.out.println("E esse Turini, está matriculado?");
+		System.out.println("\nE esse Turini, está matriculado?");
 		System.out.println(javaColecoes.estaMatriculado(turini));
 		
-		System.out.println("O a1 é equals ao Turini?");
+		System.out.println("\nO a1 é equals ao Turini?");
 		System.out.println(a1.equals(turini));
 		
 		// Sempre implementar o metodo hashcode se implementar o metodo equals
 		// obrigatoriamente o seguinte é true:
 		
 		System.out.println(a1.hashCode() == turini.hashCode());
+
+		
 		
 	}
 }
